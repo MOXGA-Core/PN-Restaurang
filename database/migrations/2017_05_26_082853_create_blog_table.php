61223940628f00c4bae8db45d7bcdb349c992a84
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2017-05-26 05:40:30 -0700

    Update

---
+++ database/migrations/2017_05_26_082853_create_blog_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('id',255)->index();
+            $table->text('text',655);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blog');
+    }
+}
